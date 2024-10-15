d01e6d94b8d0d9d18cc383f39248f4f03408a698
RascalGdd <98754472+RascalGdd@users.noreply.github.com>
2024-10-15 20:16:38 +0800

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from .detr import build
-from .deformable_detr import build as build_deformable
+# from .deformable_detr import build as build_deformable
 
 def build_model(args):
     if args.deformable_detr:
